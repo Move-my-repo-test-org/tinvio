6d30a743ba38b49bcf70baf9d0eb5d8a50179fc7
NastiaKoval <anasteziyam@gmail.com>
2020-10-29 16:21:02 +0200

    feat: make a top banner component

---
+++ src/components/TopBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TopBanner() {
+    return(
+        <section className="top-banner">
+            <div className="container">
+                <p className="overhead">FOR A SMARTER SUPPLY CHAIN</p>
+                <h1 className="main-header">Take your business to the next level with Tinvio’s Supplier Platform</h1>
+                <button className="getStarted-btn">Get Started Now</button>
+                <p className="comment">Yes, it’s free 🎉</p>
+            </div>
+        </section>
+    )
+}
+
+export default TopBanner;
