10709ffd4b2e1a13dc11a34b1f0d70a37d83bd08
NastiaKoval <anasteziyam@gmail.com>
2020-11-12 01:04:39 +0200

    feat: make styles for communication section

--- src/components/Communication.js
+++ src/components/Communication.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import communicationImg from '../assets/images/communication-image.png';
-import goodCommunication from '../assets/icons/heart.png';
+import goodCommunication from '../assets/images/goodCommunication-image.png';
+import '../css/communication.css';
 
 function Communication() {
 
@@ -9,7 +10,9 @@ function Communication() {
     return (
         <section className="communication">
             <div className="container">
-                <img className="communication-img" src={ communicationType === 'before' ? communicationImg : goodCommunication } alt="screenshot of communication" />
+                <div className="communication-img-container">
+                    <img className={communicationType === 'before' ? "communication-img" : "goodCommunication-img"} src={ communicationType === 'before' ? communicationImg : goodCommunication } alt="screenshot of communication" />
+                </div>
                 <div className="communication-text-block">
                     <h2 className="section-header communication-header">Communication</h2>
                     <div className="communication-btns-block">
@@ -18,7 +21,7 @@ function Communication() {
                     </div>
                     <hr />
                     <h3 className="section-subtitle communication-subtitle">{ communicationType === 'before' ? "Old-School Chaos" : "New method" }</h3>
-                    <p className="section-text communication-text">{ communicationType === 'before' ? "Unstructured orders across multiple apps and inboxes. New message notifications and alerts every few minutes" : "Structured orders from one app" }</p>
+                    <p className="section-text communication-text">{ communicationType === 'before' ? "Unstructured orders across multiple apps and inboxes. New message notifications and alerts every few minutes" : "Structured orders income from one app together without any spam" }</p>
                 </div>
             </div>
         </section>
