eb62a277b685afe5585fc38600231bf926dbb794
NastiaKoval <anasteziyam@gmail.com>
2020-11-11 22:35:58 +0200

    feat: make styles for preview section

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -6,13 +6,14 @@ import joliebean from '../assets/images/image-logo-bottom-joliebean.png';
 import bensCookies from '../assets/images/image-logo-bottom-bens-cookies.png';
 import simplyWraps from '../assets/images/image-logo-bottom-simply-wraps.png';
 import italianClub from '../assets/images/image-logo-bottom-italian-club.png';
+import '../css/preview.css';
 
 function PreviewSection() {
     return (
         <section className="preview">
             <div className="container">
                 <img className="desktopScreen-img" src={ desktopScreen } alt="desktop screenshot of the app" />
-                <h2 className="section-header">Trusted by 1000+ partners across APAC</h2>
+                <h2 className="section-header preview-section-header">Trusted by 1000+ partners across APAC</h2>
                 <ul className="partners-logos-list">
                     <li className="partners-logo"><img className="partners-logo-img" src={ chicken } alt="go chicken logo" /></li>
                     <li className="partners-logo"><img className="partners-logo-img" src={ joliebean } alt="joliebean logo" /></li>
