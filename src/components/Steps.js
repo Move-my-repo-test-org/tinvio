c7722ef7a1f16ea572bd7d419a1de21b3e4d7ac1
NastiaKoval <anasteziyam@gmail.com>
2020-11-13 02:39:40 +0200

    feat: make styles for steps section

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -2,6 +2,7 @@ import React from 'react';
 import question from '../assets/icons/question.png';
 import person from '../assets/icons/person+.png';
 import heart from '../assets/icons/heart.png';
+import '../css/steps.css';
 
 function Steps() {
     return (
@@ -15,15 +16,17 @@ function Steps() {
                         <h3 className="section-subtitle steps-subtitle">Get in Touch With Us</h3>
                         <p className="step-text">Fill up the form and we’ll reach out to you</p>
                     </div>
+                    <div className="steps-line"></div>
                     <div className="step-wrapper">
                         <img className="step-img" src={ person } alt="person icon" />
                         <h3 className="section-subtitle steps-subtitle">Connect to Your Clients</h3>
-                        <p className="step-text">They’ll receive a fully guided tutorial on how to order</p>
+                        <p className="step-text second-step-text">They’ll receive a fully guided tutorial on how to order</p>
                     </div>
+                    <div className="steps-line"></div>
                     <div className="step-wrapper">
                         <img className="step-img" src={ heart } alt="heart icon" />
                         <h3 className="section-subtitle steps-subtitle">Onboarded In a Heartbeat</h3>
-                        <p className="step-text">Activated and live within a day</p>
+                        <p className="step-text third-text-step">Activated and live within a day</p>
                     </div>
                 </div>
             </div>
