6c6f37042fb6fb0d6f9077ec5fb55e9cff355246
NastiaKoval <anasteziyam@gmail.com>
2020-11-13 14:26:55 +0200

    feat: make styles for getStarted section

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -3,6 +3,7 @@ import CountriesList from './CountriesList';
 import Form from './Form';
 import rightArrow from '../assets/icons/right-arrow-handwritten.png';
 import leftArrow from '../assets/icons/left-arrow-handwritten.png';
+import '../css/getStarted.css';
 
 
 
@@ -12,16 +13,16 @@ function GetStarted() {
             <div className="container">
                 <div className="getStarted-header-block">
                     <p className="overhead getStarted-overhead">let’s do this</p>
-                    <h2 className="section-header steps-header">Fill up the form and we’ll get in touch within a few hours</h2>
+                    <h2 className="section-header getStarted-header">Fill up the form and we’ll get in touch within<br /> a few hours</h2>
                 </div>
                 <div className="getStarted-form-block">
-                    <p className="form-text handwritten-text">Or meet us in</p>
                     <img className="form-arrow" src={ leftArrow } alt="left handwritten arrow" />
+                    <p className="form-text handwritten-text">Let’s get to know each other</p>
                     <Form />
                 </div>
                 <div className="meet-us">
-                    <p className="meet-us-text handwritten-text">Or meet us in</p>
                     <img className="meet-us-arrow" src={ rightArrow } alt="right handwritten arrow" />
+                    <p className="meet-us-text handwritten-text">Or meet us in</p>
                     <CountriesList />
                 </div>
             </div>
