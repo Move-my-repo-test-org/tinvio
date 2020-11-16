9d372dae64425c8d470c49c7cb4301392021437c
NastiaKoval <anasteziyam@gmail.com>
2020-11-16 16:57:37 +0200

    feat: make a burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,11 @@ function Header() {
 
     const [deviceType, setDeviceType] = React.useState("");
     const [currentPage, setCurrentPage] = React.useState("Home");
+    const [burgerOpened, setBurgerOpened] = React.useState(false);
     const defineDeviceType = () => {
         if (window.innerWidth >= 768) {
             setDeviceType('Desktop');
+            setBurgerOpened(false);
         } else {
             setDeviceType('Mobile');
         }
@@ -28,24 +30,43 @@ function Header() {
     }
     
     return (
-        <header className="header">
+        <header className={burgerOpened ? "header header-opened-burger" : "header"} >
             <div className="container">
                 <nav className="nav">
-                    <a href="./" className="logo"><img src={ logo } className="logo-img" alt="logo" /></a>
-                    <div className="header-vertical-line"></div>
-                    {deviceType==='Desktop'? 
-                        (<div className="desktop-menu">
-                            <div className="menu-container" onClick={makeActive}>
-                                <a href="./" className={ currentPage==='Home' ? "active menu-item" : "menu-item" } >Home</a>
-                                <a href="./supplier" className={ currentPage==='Supplier' ? "active menu-item" : "menu-item" } >Supplier</a>
-                                <a href="./about" className={ currentPage==='About' ? "active menu-item" : "menu-item" } >About</a>
-                                <a href="./contact" className={ currentPage==='Contact' ? "active menu-item" : "menu-item" } >Contact</a>
+                    <div className="nav-line">
+                        <a href="./" className="logo"><img src={ logo } className="logo-img" alt="logo" /></a>
+                        <div className="header-vertical-line"></div>
+                        { deviceType==='Desktop'? 
+                            (<div className="desktop-menu">
+                                <div className="menu-container" onClick={makeActive}>
+                                    <a href="./" className={ currentPage==='Home' ? "active menu-item" : "menu-item" } >Home</a>
+                                    <a href="./supplier" className={ currentPage==='Supplier' ? "active menu-item" : "menu-item" } >Supplier</a>
+                                    <a href="./about" className={ currentPage==='About' ? "active menu-item" : "menu-item" } >About</a>
+                                    <a href="./contact" className={ currentPage==='Contact' ? "active menu-item" : "menu-item" } >Contact</a>
+                                </div>
+                                <button className="header-btn" onClick={() => {goToAnchor('get-started')}}>Get Started</button>
+                            </div>) :
+                            <div className="mobile-menu">
+                                { burgerOpened ? 
+                                    <button className="burger-btn-close" onClick={ () => {setBurgerOpened(false)} } ></button>
+                                    :
+                                    <img src={ burger } className="burger-img" alt="burger icon" onClick={ () => {setBurgerOpened(true)} } />
+                                }
+                            </div>
+                        }
+                    </div>
+                    { burgerOpened ? 
+                        ( <div className="burger-menu" onClick={ () => {setBurgerOpened(false)} }>
+                            <div className="burger-menu-container" >
+                                    <a href="./" className={ currentPage==='Home' ? "active menu-item" : "menu-item" } >Home</a>
+                                    <a href="./supplier" className={ currentPage==='Supplier' ? "active menu-item" : "menu-item" } >Supplier</a>
+                                    <a href="./about" className={ currentPage==='About' ? "active menu-item" : "menu-item" } >About</a>
+                                    <a href="./contact" className={ currentPage==='Contact' ? "active menu-item" : "menu-item" } >Contact</a>
                             </div>
                             <button className="header-btn" onClick={() => {goToAnchor('get-started')}}>Get Started</button>
-                        </div>) :
-                        <div className="mobile-menu">
-                            <img src={ burger } className="burger-img" alt="burger icon" />
-                        </div>
+                        </div>)
+                        : 
+                        null
                     }
                 </nav>
             </div>
